e286112958af3b9f83e2d2ac7407bce5c2018fc5
Chimpook Virtuch <chimpook@gmail.com>
2022-05-09 19:44:21 +0200

    Create a controller

---
+++ app/src/Controller/QuoteController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\QuoteRepository;
+
+class QuoteController extends AbstractController
+{
+    #[Route('/', name: 'index')]
+    public function index(QuoteRepository $quoteRepository): Response
+    {
+        return $this->render(
+            'quote/index.html.twig',
+            [
+                'quotes' => $quoteRepository->findAll()
+            ]
+        );
+    }
+
+    
+}
